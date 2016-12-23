b18200c5338a6b5ce1b0cbfe928ec0590e72209a
Darrell Wright <dwright-git@dawdevel.ca>
2016-12-23 05:42:10 +0000

    cleanup

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -8,24 +8,27 @@
 
 using real_t = boost::multiprecision::number<boost::multiprecision::cpp_dec_float<10240>>;
 
-real_t operator"" _R( long double d ) noexcept {
+real_t operator"" _R( long double d ) {
 	return real_t{ d };
 }
 
-real_t get_fibonacci( uintmax_t const n ) noexcept {
+real_t get_fibonacci( uintmax_t const n ) {
 	// Use Binet's formula, limited n to decimal for perf increase
 	if( n < 1 ) {
 		return 0.0_R;
 	}
 	static auto const sqrt_five = sqrt( 5.0_R );
-	auto const b = pow( (1.0_R + sqrt_five)/2.0_R, n );
-	auto const c = pow( (1.0_R - sqrt_five)/2.0_R, n );
+	static auto const b_part = (1.0_R + sqrt_five) / 2.0_R;
+	static auto const c_part = (1.0_R - sqrt_five) / 2.0_R;
+
+	auto const b = pow( b_part, n );
+	auto const c = pow( c_part, n );
 	return round( (b - c)/sqrt_five );
 }
 
 int main( int argc, char **argv ) { 
 	assert( argc == 2 );
-	uintmax_t n = strtoull( argv[1], 0, 10 );
+	auto n = strtoull( argv[1], 0, 10 );
 	std::cout << n << ": " << std::setprecision(std::numeric_limits<real_t>::max_digits10) << get_fibonacci( n ) << '\n';
 	return EXIT_SUCCESS;
 }
