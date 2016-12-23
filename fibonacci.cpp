f855f73603508fe90479e24222571c270f028e37
Darrell Wright <dwright-git@dawdevel.ca>
2016-12-23 05:13:01 +0000

    cleanup

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -18,10 +18,9 @@ real_t get_fibonacci( uintmax_t const n ) noexcept {
 		return 0.0_R;
 	}
 	static auto const sqrt_five = sqrt( 5.0_R );
-	static auto const a = 1.0_R/sqrt_five;
 	auto const b = pow( (1.0_R + sqrt_five)/2.0_R, n );
 	auto const c = pow( (1.0_R - sqrt_five)/2.0_R, n );
-	return round( a*(b - c) );
+	return round( (b - c)/sqrt_five );
 }
 
 int main( int argc, char **argv ) { 
